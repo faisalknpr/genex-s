2c23a1971f144dccb969abdf7be8cd90acc0b18b
faisalknpr <er.khanfaisal@gmail.com>
2023-02-18 01:57:19 +0530

    final

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const multer = require('multer');
 const path = require('path');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const fs = require('fs');
+const serveStatic = require('serve-static');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(serveStatic('decrypted'));
+app.set('view engine', 'ejs');
 // using the Node.js built-in crypto module to encrypt the files using the AES-256-CBC algorithm
 
 
 let algorithm;
 let key;
 let iv;
+
 function generateKeyandIv() {
   algorithm = 'aes-256-cbc';
   key = crypto.randomBytes(32); // key should be stored in mongoDB
@@ -25,7 +31,9 @@ function generateKeyandIv() {
 
 //mongoDB connection
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/filekey', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/filekey', {
+  useNewUrlParser: true
+});
 const fileSchema = new mongoose.Schema({
   fileName: {
     type: String,
@@ -40,6 +48,8 @@ const fileSchema = new mongoose.Schema({
     required: true,
   },
 });
+// create mongoose model
+const File = mongoose.model('File', fileSchema);
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
@@ -47,7 +57,7 @@ const storage = multer.diskStorage({
     cb(null, 'uploads');
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    cb(null, file.originalname);
   },
 });
 
@@ -67,34 +77,43 @@ function checkFileType(file, cb) {
   // Check mime
   const mimetype = filetypes.test(file.mimetype);
 
-    if (mimetype && extname) {
+  if (mimetype && extname) {
     return cb(null, true);
   } else {
     cb('Error: Images Only!');
   }
 }
-    app.post('/upload', (req, res) => {
-      upload(req, res, async (err) => {
-        if (err)
-        {
-      res.status(400).json({ msg: err });
+app.post('/upload', (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      res.status(400).json({
+        msg: err
+      });
     } else {
       if (req.files === undefined) {
-        res.status(400).json({ msg: 'No file selected' });
+        res.status(400).json({
+          msg: 'No file selected'
+        });
       } else {
 
         const encryptedFiles = [];
         for (const f of req.files) {
           const encryptedFile = await encryptFile(f);
           encryptedFiles.push(encryptedFile);
-          // Store the encrypted files in the database
-          const File = mongoose.model('File', fileSchema);
+
           // Save the file metadata in the database
-            const file = new File({ fileName:f.originalname, key:key, iv:iv });
-            file.save();
+          const file = new File({
+            fileName: f.originalname,
+            key: key.toString("base64"),
+            iv: iv.toString("base64")
+          });
+          file.save();
         }
 
-        res.json({ msg: 'File uploaded', files: encryptedFiles });
+        res.json({
+          msg: 'File uploaded',
+          files: encryptedFiles
+        });
       }
     }
   });
@@ -133,27 +152,31 @@ function encryptFile(file) {
   });
 }
 
-// // function for decryption
-//
-// function decryptFile(encryptedFilePath) {
-//   const encryptedReadStream = fs.createReadStream(encryptedFilePath);
-//   const decipher = crypto.createDecipheriv(algorithm, key, iv);
-//   const decryptedName = encryptedFilePath.slice(0, -4);
-//   const decryptedWriteStream = fs.createWriteStream(decryptedName);
-//
-//   encryptedReadStream
-//     .pipe(decipher)
-//     .pipe(decryptedWriteStream)
-//     .on('finish', () => {
-//       console.log(`Decrypted file: ${decryptedName}`);
-//     });
-// }
-//
-//
-//
-// // Retrieve the file metadata from the database
-//
-// const getFileMetadata = async (fileName) => {
-//   const file = await File.findOne({ fileName });
-//   return file;
-// };
+app.get('/admin', async function(req, res) {
+  const data = await File.find();
+  data.forEach((item) => {
+    const directoryPath = path.join(__dirname, 'uploads', item.fileName + ".enc");
+    console.log(directoryPath, item.key, item.iv);
+    decryptFile(directoryPath, item.key.toString("base64"), item.iv.toString("base64"), "C:\Users\faisa\desktop\genex-s\decrypted");
+  });
+  res.render('admin', {
+    data
+  });
+});
+
+// function for decryption
+
+function decryptFile(encryptedFilePath, key, iv, outputDir) {
+  const algorithm = 'aes-256-cbc';
+  const encryptedReadStream = fs.createReadStream(encryptedFilePath);
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'base64'), Buffer.from(iv, 'base64'));
+  const decryptedName = path.join(__dirname, "decrypted", path.parse(encryptedFilePath).name);
+  const decryptedWriteStream = fs.createWriteStream(decryptedName);
+
+  encryptedReadStream
+    .pipe(decipher)
+    .pipe(decryptedWriteStream)
+    .on('finish', () => {
+      console.log(`Decrypted file: ${decryptedName}`);
+    });
+}
